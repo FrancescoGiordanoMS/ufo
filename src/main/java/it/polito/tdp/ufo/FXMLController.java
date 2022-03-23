2f5b4d980e9e66e54cdd42567b776ee932af853c
francesco <francesco@192.168.1.135>
2022-03-23 19:51:34 +0100

    Modifiche del 2022.03.23

--- src/main/java/it/polito/tdp/ufo/FXMLController.java
+++ src/main/java/it/polito/tdp/ufo/FXMLController.java
@@ -1,10 +1,14 @@
 package it.polito.tdp.ufo;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.ufo.model.Model;
 import it.polito.tdp.ufo.model.Sighting;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,39 +18,85 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLController {
-	
-	private Model model ;
 
-    @FXML
-    private ResourceBundle resources;
+	private Model model;
+	Integer index = -1;
 
-    @FXML
-    private URL location;
-
-    @FXML
-    private ComboBox<String> boxForma;
-
-    @FXML
-    private Label txtMessaggio;
-    
-    @FXML
+	@FXML
     private TableView<Sighting> TVUfo;
-
+    
     @FXML
     private TableColumn<Sighting, Integer> col_id;
 
     @FXML
     private TableColumn<Sighting, String> col_city;
-       
+
     @FXML
-    private TableColumn<Sighting, String> col_shape;
-    
+    private Button PBMod;
+
+    @FXML
+    private Label txtMessaggio;
+
+    @FXML
+    private ComboBox<String> boxForma;
+
+    @FXML
+    private Button PBAdd;
+
+    @FXML
+    private TextField TFId;
+
+    @FXML
+    private TextField TFCity;
+
+    @FXML
+    private TextField TFCity1;
+
     @FXML
     private Button PBCaricaDati;
-  
+
+    @FXML
+    private TableColumn<Sighting, String> col_shape;
+
+    @FXML
+    private TextField TFShape;
+
+
+    
+    @FXML
+    void handleAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HMod(ActionEvent event) {
+
+    }
+
+   private final StringProperty twoWayInput = new SimpleStringProperty();
+    public StringProperty twoWayInputProperty() {return twoWayInput;}
+    @FXML
+    void getSelected(MouseEvent event) {
+    	index = TVUfo.getSelectionModel().getSelectedIndex();
+    	if (index <= -1)
+    		return;
+    	TFId.setText(col_id.getCellData(index).toString());
+    	TFCity.setText(col_city.getCellData(index).toString());
+    	TFShape.setText(col_shape.getCellData(index).toString());
+    	
+    	//TFCity.textProperty().bind(twoWayInputProperty());
+    	Bindings.bindBidirectional(twoWayInputProperty(), TFCity1.textProperty());
+    	Bindings.bindBidirectional(twoWayInputProperty(), TFCity.textProperty());
+    	//Bindings.bindBidirectional(twoWayInputProperty(), col_city.textProperty());
+    	//TVUfo.getSelectionModel().selectedItemProperty().addListener(observableValue, authorProps, authorProps2);
+    }
+    
+    
     
     @FXML
     void handleConta(ActionEvent event) {
@@ -70,9 +120,6 @@ public class FXMLController {
     
     public void TableView() {
         obs=model.getRighe();
-        
-
-        
         this.TVUfo.setItems(obs);
     }
     
@@ -86,6 +133,6 @@ public class FXMLController {
         col_shape.setCellValueFactory(new PropertyValueFactory<Sighting,String>("shape"));
            
     }
-
-	
+  
 }
+
